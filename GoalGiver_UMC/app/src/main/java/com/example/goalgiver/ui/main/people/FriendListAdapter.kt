162314cd1fc45629573f9aa2a31ff08a336b22ea
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2024-08-19 19:53:49 +0900

    friendlist 친구 삭제 버튼

--- GoalGiver_UMC/app/src/main/java/com/example/goalgiver/ui/main/people/FriendListAdapter.kt
+++ GoalGiver_UMC/app/src/main/java/com/example/goalgiver/ui/main/people/FriendListAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.goalgiver.ui.main.people
 
+import android.content.DialogInterface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.goalgiver.R
+import org.w3c.dom.Text
 
 class FriendListAdapter(val itemList: ArrayList<FriendItem>): RecyclerView.Adapter<FriendListAdapter.FriendListViewHolder>() {
 
@@ -21,6 +24,22 @@ class FriendListAdapter(val itemList: ArrayList<FriendItem>): RecyclerView.Adapt
     override fun onBindViewHolder(holder: FriendListViewHolder, position: Int) {
         holder.tv_name.text = itemList[position].name
         holder.tv_img.setImageResource(R.drawable.img_books)
+        holder.tv_delete.setOnClickListener {
+            val builder = AlertDialog.Builder(holder.itemView.context)
+            builder.setMessage("친구 목록에서 삭제할까요?")
+                .setPositiveButton("확인",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        itemList.removeAt(position)
+                        notifyItemRemoved(position)
+                        notifyItemRangeChanged(position, itemList.size)
+                    })
+                .setNegativeButton("취소",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        dialog.dismiss()
+                    })
+
+            builder.show()
+        }
     }
 
     override fun getItemCount(): Int {
@@ -30,5 +49,6 @@ class FriendListAdapter(val itemList: ArrayList<FriendItem>): RecyclerView.Adapt
     inner class FriendListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val tv_name = itemView.findViewById<TextView>(R.id.tv_friendlist_name)
         val tv_img = itemView.findViewById<ImageView>(R.id.iv_friendlist_profile)
+        val tv_delete = itemView.findViewById<TextView>(R.id.tv_friendlist_delete)
     }
 }
