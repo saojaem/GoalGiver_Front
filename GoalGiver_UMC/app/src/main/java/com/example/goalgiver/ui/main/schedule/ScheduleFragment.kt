b80ff1866b8acb9edba4c162083e45f19ad65bf2
saojaem <saojaem@gmail.com>
2024-08-21 15:14:51 +0900

    fix오류수정

--- GoalGiver_UMC/app/src/main/java/com/example/goalgiver/ui/main/schedule/ScheduleFragment.kt
+++ GoalGiver_UMC/app/src/main/java/com/example/goalgiver/ui/main/schedule/ScheduleFragment.kt
@@ -35,7 +35,7 @@ class ScheduleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
-        handleIncomingIntentData() // Intent 데이터 처리
+       // handleIncomingIntentData() // Intent 데이터 처리
         loadInitialData() // 초기 데이터 로드
     }
 
@@ -59,20 +59,20 @@ class ScheduleFragment : Fragment() {
         }
     }
 
-    private fun handleIncomingIntentData() {
-        arguments?.let { bundle ->
-            val toDoItem = bundle.getParcelable<ToDoItem>("toDoItem")
-
-            toDoItem?.let {
-                initialToDoList.add(it)
-                todoAdapter.submitList(initialToDoList.toList())
-                todoAdapter.notifyDataSetChanged()
-
-                val dates = getDatesBetween(it.startdate, it.enddate)
-                binding.calendarView.addDecorator(DotDecorator(requireContext(), dates))
-            }
-        }
-    }
+//    private fun handleIncomingIntentData() {
+//        arguments?.let { bundle ->
+//            val toDoItem = bundle.getParcelable<ToDoItem>("toDoItem")
+//
+//            toDoItem?.let {
+//                initialToDoList.add(it)
+//                todoAdapter.submitList(initialToDoList.toList())
+//                todoAdapter.notifyDataSetChanged()
+//
+//                val dates = getDatesBetween(it.startdate, it.enddate)
+//                binding.calendarView.addDecorator(DotDecorator(requireContext(), dates))
+//            }
+//        }
+//    }
 
     private fun loadInitialData() {
         if (initialToDoList.isEmpty()) {
